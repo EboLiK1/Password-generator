1469037d10f73db00f263b01b9306802344a7e8c
ebolik11 <romamazavak.m@gmail.com>
2022-08-17 18:31:34 +0300

    Добавьте файлы проекта.

---
+++ Генератор паролей C/Генератор паролей C++.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+
+    int count;
+    int i = 0;
+    int b = 0;
+    char ch;
+    string password;
+
+    cout << "Введите число символов в пароле: ";
+    cin >> count;
+
+    while (b < 5)
+    {
+        password = "";
+        while (i < count)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            ch = alphabet[rand() % alphabet.size()];
+            password += ch;
+
+            i++;
+        }
+        i = 0;
+        cout << password << endl;
+        b++;
+    }
+}
